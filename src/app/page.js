b2a5420a995eda2724dd63c77a601f7380d74fb8
kurmananda <edu.kurmananda@gmail.com>
2026-05-12 14:57:41 +0530

    date chnges

--- src/app/page.js
+++ src/app/page.js
@@ -138,7 +138,7 @@ export default function Home() {
     {
       title: 'Cube Sat Workshop',
       price: 349,
-      desc: '20th April 2026',
+      desc: '20th June 2026',
       details : ['1. End to end concepts of Cubesat design', '2. Learn from experts who have worked on in-flight satellites.', '3. Understand mission design and space fundamentals'],
       icon: <Satellite size={20} />,
       cat : 'space',
@@ -146,7 +146,7 @@ export default function Home() {
     {
       title: 'Launch Vehicle Workshop',
       price: 349,
-      desc: '27th April 2026',
+      desc: '27th June 2026',
       details : ['1. End to end concepts of Launch Vehicles', '2. Understand mission design and space fundamentals', '3. Explore propulsion, staging, and flight dynamics basics'],
       icon: <Rocket size={20} />,
       cat : 'space',
@@ -154,7 +154,7 @@ export default function Home() {
     {
       title: 'Agentic AI Workshop',
       price: 299,
-      desc: '28th April 2026',
+      desc: '28th June 2026',
       details : ['1. Explore the basics of prompt engineering', '2. Optimize AI usage for maximum productivity', '3. Hands on learning with AI agents.'],
       icon: <Cpu size={20} />,
       cat : 'tech',
@@ -162,7 +162,7 @@ export default function Home() {
     {
       title: 'Python ML Workshop',
       price: 299,
-      desc: '21st April 2026',
+      desc: '21st June 2026',
       details : ['1. Explore the basics of python and machine learning', '2. Understand concepts with application focused learning', '3. Develop industry focused skills.'],
       icon: <Brain size={20} />,
       cat : 'tech',
